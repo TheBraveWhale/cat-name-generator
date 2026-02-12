0f0439826e8028ecc2f59cd5ef548b36ef0d098b
TheBraveWhale <isavdlaan@hotmail.com>
2026-02-12 12:12:17 +0100

    added axios + basic js for generating the ai  catnames

--- src/script.js
+++ src/script.js
@@ -1,13 +1,29 @@
-function generateNames(event) {
-  event.preventDefault();
-
+function generateNames(response) {
   new Typewriter("#aiOutput", {
-    strings: "Catname",
+    strings: response.data.answer,
     autoStart: true,
-    delay: 100,
+    delay: 10,
     cursor: "",
   });
 }
 
+function callAI(event) {
+  event.preventDefault();
+
+  let userInput = document.querySelector("#input");
+
+  let apiKey = "fa25b40fcffa301t230o795c80b7e53f";
+  let prompt = `Please generate 5 cat names based on these keywords provided to descripe the cat:${userInput.value}. please provide only the names following this html set up: "<span class="material-symbols-outlined"> pets </span>
+      Catname <br />
+      <span class="material-symbols-outlined"> pets </span>
+      Catname <br />
+      <span class="material-symbols-outlined"> pets </span>
+      Catname"`;
+  let context = "You are an expert on cat names";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(generateNames);
+}
+
 let catNamesForm = document.querySelector("#inputForm");
-catNamesForm.addEventListener("submit", generateNames);
+catNamesForm.addEventListener("submit", callAI);
