1d2b4ff13081a294a98d167cdd13e3b66bb17778
TheBraveWhale <isavdlaan@hotmail.com>
2026-02-12 13:08:19 +0100

    added some user experience enhancers + some tidy ups
    added the ai results box that is hidden till it generates. Whilst generating the box tells u this and a nice loading animation box appears on the screen.
    
    the prompt has been adjusted to include both short and longer names

--- src/script.js
+++ src/script.js
@@ -13,7 +13,7 @@ function callAI(event) {
   let userInput = document.querySelector("#input");
 
   let apiKey = "fa25b40fcffa301t230o795c80b7e53f";
-  let prompt = `Please generate 5 cat names based on these keywords provided to descripe the cat:${userInput.value}. please provide only the names following this html set up: "<span class="material-symbols-outlined"> pets </span>
+  let prompt = `Please generate 2 1 word cat names and 3 2 word cat names based on these keywords provided to descripe the cat:${userInput.value}. please provide only the names following this set up: "<span class="material-symbols-outlined"> pets </span>
       Catname <br />
       <span class="material-symbols-outlined"> pets </span>
       Catname <br />
@@ -21,6 +21,20 @@ function callAI(event) {
       Catname"`;
   let context = "You are an expert on cat names";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let aiOutputElement = document.querySelector("#aiOutput");
+  aiOutputElement.classList.remove("hidden");
+  aiOutputElement.innerHTML = `<div class="loader">
+  <div class="square" id="sq1"></div>
+  <div class="square" id="sq2"></div>
+  <div class="square" id="sq3"></div>
+  <div class="square" id="sq4"></div>
+  <div class="square" id="sq5"></div>
+  <div class="square" id="sq6"></div>
+  <div class="square" id="sq7"></div>
+  <div class="square" id="sq8"></div>
+  <div class="square" id="sq9"></div>
+</div>Generating cat names...`;
 
   axios.get(apiUrl).then(generateNames);
 }
